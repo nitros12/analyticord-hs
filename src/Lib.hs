6c7267a7ba4703b40b8c1a4bd9e9c969b833c6dc
ben simms <ben@bensimms.moe>
2017-08-24 17:09:58 +0100

    Add some stuff to docs

--- src/Lib.hs
+++ src/Lib.hs
@@ -22,12 +22,13 @@ import           Data.Aeson                (FromJSON, eitherDecode)
 
 import           DataTypes
 
-type ApiEffect a = IO (Either ApiErr a)
+type APIresponse a = IO (Either ApiErr a)
 
 
 data ApiKey = ApiKey { bot  :: ByteString
                      , user :: Maybe ByteString
                      } deriving Show
+
 get =  (++) "GET "
 post = (++) "POST "
 
@@ -42,15 +43,17 @@ getAddress     = get  $ baseAddress // "api" // "getData"
 botinfoAddress = get  $ baseAddress // "api" // "botinfo"
 botlistAddress = get  $ baseAddress // "api" // "botlist"
 
+
 -- |Build an api key from the bot key, and maybe the user key
 makeKey :: ByteString -> Maybe ByteString -> ApiKey
 makeKey bot user = ApiKey (B.append "bot " bot) (B.append "user " <$> user)
+
 
 -- |Make a request to the API
 makeRequest :: FromJSON a => String -- ^ The url to make the request to, in the format "\<request type\> \<url\>"
             -> [(ByteString, Maybe ByteString)] -- ^ Array of k:v pairs for request parameters
             -> ByteString -- ^ Auth string to use
-            -> ApiEffect a -- ^ Returned API response, either an error or your response
+            -> APIresponse a -- ^ Returned API response, either an error or your response
 makeRequest url params auth = do
   manager <- newManager tlsManagerSettings
 
@@ -61,27 +64,37 @@ makeRequest url params auth = do
   let resp = responseBody res
   let status = statusCode $ responseStatus res
   pure $ case status of
-    200 -> case eitherDecode resp of
-      Right a  -> Right a
-      Left err -> Prelude.error ("Unable to decode:" ++ err)
-    otherwise -> case eitherDecode resp of
-      Right a  -> Left a
-      Left err -> Prelude.error ("Unable to decode:" ++ err)
+    200 -> either (Prelude.error . ("Unable to decode: " ++)) pure $ eitherDecode resp
+    otherwise -> either (Prelude.error . ("Unable to decode: " ++)) Left $ eitherDecode resp
 
-doLogin :: ApiKey -> ApiEffect LoginResponse
+
+-- |Hit the login endpoint.
+doLogin :: ApiKey -> APIresponse LoginResponse
 doLogin = (makeRequest loginAddress []) . bot
 
-doSend :: ApiKey -> ByteString -> ByteString -> ApiEffect SendResponse
+
+-- |Send some data to the api
+doSend :: ApiKey
+       -> ByteString -- ^ Event data type
+       -> ByteString -- ^ Data to send
+       -> APIresponse SendResponse
 doSend key etype edata = makeRequest sendAddress [(etype, Just edata)] $ bot key
 
-doGet :: ApiKey -> [(ByteString, Maybe ByteString)] -> ApiEffect [DataResponse]
+
+-- |Get data from the api
+doGet :: ApiKey
+      -> [(ByteString, Maybe ByteString)] -- ^ k:v pairs of query strings
+      -> APIresponse [DataResponse]
 doGet (ApiKey _ (Just key)) params = makeRequest getAddress params key
 doGet (ApiKey _ Nothing) _ = Prelude.error "ApiKey must be given a user key to call user endpoints"
 
-doBotList :: ApiKey -> ApiEffect [BotResponse]
+
+-- |Get a list of bots owned by your key
+doBotList :: ApiKey -> APIresponse [BotResponse]
 doBotList (ApiKey _ (Just key)) = makeRequest botlistAddress [] key
 doBotList (ApiKey _ Nothing) = Prelude.error "ApiKey must be given a user key to call user endpoints"
 
-doBotInfo :: ApiKey -> Text -> ApiEffect [BotResponse]
+-- |Get bot info for a bot id
+doBotInfo :: ApiKey -> Text -> APIresponse [BotResponse]
 doBotInfo (ApiKey _ (Just key)) id = makeRequest botinfoAddress [("id", Just . encodeUtf8 $ id)] key
 doBotInfo (ApiKey _ Nothing) _ = Prelude.error "ApiKey must be given a user key to call user endpoints"
